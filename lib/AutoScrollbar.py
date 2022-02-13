63beba62bf23b39b081f00bde4d96740570c87c9
DoubleYouC <WendellCarper@hotmail.com>
2022-02-12 19:46:13 -0500

    Code cleanup

--- lib/AutoScrollbar.py
+++ lib/AutoScrollbar.py
@@ -1,15 +1,18 @@
+"""This is the AutoScrollbar module."""
+
 import tkinter as tk
 
 class AutoScrollbar(tk.Scrollbar):
-    def set(self, lo, hi):
-        if float(lo) <= 0.0 and float(hi) >= 1.0:
+    """This creates a scrollbar if necessary."""
+    def set(self, first, last):
+        if float(first) <= 0.0 and float(last) >= 1.0:
             self.pack_forget()
         else:
             if self.cget("orient") == 'horizontal':
                 self.pack(side=tk.BOTTOM, fill=tk.X)
             else:
                 self.pack(side=tk.RIGHT, fill=tk.Y)
-        tk.Scrollbar.set(self, lo, hi)
+        tk.Scrollbar.set(self, first, last)
 
 if __name__ == '__main__':
     print('This is the AutoScrollbar module.')
