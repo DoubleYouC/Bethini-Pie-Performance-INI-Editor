8b43772dc2b415cb619699c1603d348e9d630cce
Michael <wxMichael@users.noreply.github.com>
2025-02-11 11:46:17 -0500

    Reduce nesting

--- lib/AutoScrollbar.py
+++ lib/AutoScrollbar.py
@@ -7,11 +7,10 @@ class AutoScrollbar(tk.Scrollbar):
     def set(self, first, last):
         if float(first) <= 0.0 and float(last) >= 1.0:
             self.pack_forget()
+        elif self.cget("orient") == 'horizontal':
+            self.pack(side=tk.BOTTOM, fill=tk.X)
         else:
-            if self.cget("orient") == 'horizontal':
-                self.pack(side=tk.BOTTOM, fill=tk.X)
-            else:
-                self.pack(side=tk.RIGHT, fill=tk.Y)
+            self.pack(side=tk.RIGHT, fill=tk.Y)
         tk.Scrollbar.set(self, first, last)
 
 if __name__ == '__main__':
