fc6aaf0f119bfc6bcedfc095741d712ae70809ba
Michael <wxMichael@users.noreply.github.com>
2025-02-24 13:05:43 -0500

    Update scalar.py

--- lib/scalar.py
+++ lib/scalar.py
@@ -20,7 +20,7 @@ class Scalar(ttk.Scale):
     def __init__(
         self,
         master: tk.Misc | None = None,
-        command: str | Callable[[str], object] = "",
+        # command: str | Callable[[str], object] = "",
         from_: float = 0,
         length: int = 100,
         orient: Literal["horizontal", "vertical"] = "horizontal",
@@ -29,15 +29,28 @@ class Scalar(ttk.Scale):
         decimal_places: IntStr = "0",
     ) -> None:
         self.decimal_places = int(decimal_places)
-        if command:
-            self.chain = command
-        else:
-            self.chain = lambda *_a: None
-        super().__init__(master, command=self._value_changed, from_=from_, length=length, orient=orient, to=to, variable=variable)
+        # Currently unused. Supports the above commented command parameter.
+        # if command:
+        #     self.chain = command
+        # else:
+        #     self.chain = lambda *_a: None
+        super().__init__(master, command=self._value_changed, from_=from_, length=length, orient=orient, to=to)
+        self.variable = variable
+        if self.variable:
+            self.configure(variable=self.variable)
 
     def _value_changed(self, _new_value: str) -> None:
-        if self.decimal_places == 0:
-            value = int(self.get())
-        else:
-            value = round(self.get(), self.decimal_places)
-        self.chain(value)
+        if self.variable:
+            value = self.get()
+            value = int(value) if self.decimal_places == 0 else round(value, self.decimal_places)
+
+            if isinstance(self.variable, ttk.IntVar):
+                self.variable.set(int(value))
+            else:
+                self.variable.set(value)
+
+            # See above comments.
+            # if isinstance(self.chain, str):
+            #     pass
+            # else:
+            #     self.chain(value)
